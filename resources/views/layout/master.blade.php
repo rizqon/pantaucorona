6536352efcc27be6607a5919c946fd9b1d76bfdf
Rizqon Sadida <rizqoqonsadida@gmail.com>
2020-03-15 05:17:06 +0700

    [change] title website

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Top Navigation</title>
+  <title>Pantau Corona | Dashboard</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
