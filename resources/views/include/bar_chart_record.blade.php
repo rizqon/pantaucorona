e64cba0d1feee0f2fd32b0331c6ba3bdb633e3d1
Rafi Halilintar <masitingss@gmail.com>
2020-03-15 20:35:33 +0700

    [ update ] delete card tools

--- resources/views/include/bar_chart_record.blade.php
+++ resources/views/include/bar_chart_record.blade.php
@@ -3,13 +3,6 @@
         <div class="card card-success">
             <div class="card-header">
                 <h3 class="card-title">Timeline</h3>
-
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i
-                            class="fas fa-times"></i></button>
-                </div>
             </div>
             <div class="card-body">
                 <div class="chart">
--- resources/views/include/news_stream.blade.php
+++ resources/views/include/news_stream.blade.php
@@ -2,14 +2,6 @@
     <div class="card card-success">
         <div class="card-header">
             <h3 class="card-title">Berita Stream</h3>
-            <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                    <i class="fas fa-times"></i>
-                </button>
-            </div>
         </div>
         <div class="card-body p-0">
             <ul class="products-list product-list-in-card pl-2 pr-2">
